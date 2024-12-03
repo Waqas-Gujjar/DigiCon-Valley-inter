9b9285e38891c371fe164b6f08b5a258a57bbc5e
WaqasGujjar <wg4941792@gmail.com>
2024-12-04 01:08:17 +0500

    update

--- Conditionals_and_Loop/19.py
+++ Conditionals_and_Loop/19.py
@@ -1,1 +1,21 @@
 # Write a program to print the first n odd numbers.
+
+number = int(input("Enter a number: "))
+
+
+for i in range(1, 2 * number ,2):
+    print(i)
+   
+print("The first", number, "odd numbers are")
+
+# output
+# Enter a number: 8
+# 1
+# 3
+# 5
+# 7
+# 9
+# 11
+# 13
+# 15
+# The first 8 odd numbers are
